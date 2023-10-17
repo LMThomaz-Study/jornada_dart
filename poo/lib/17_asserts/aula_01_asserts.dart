953df8f8ca8330c9c1f7bc5888b668d8e3e8a229
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-10-17 20:52:42 -0300

    asserts

---
+++ poo/lib/17_asserts/aula_01_asserts.dart
@@ -0,0 +1,10 @@
+import 'package:dart_poo/17_asserts/cliente.dart';
+
+void main() {
+  var cli = Cliente(
+    cpf: '11122233344',
+    nome: 'Cleitin',
+    cnpj: '111',
+    razaoSocial: '1111',
+  );
+}
