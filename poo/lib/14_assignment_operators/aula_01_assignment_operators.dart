ab91ce27445335e45aeb043839ab9b45d0b10aa6
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-10-17 16:41:19 -0300

    Assignment operators

---
+++ poo/lib/14_assignment_operators/aula_01_assignment_operators.dart
@@ -0,0 +1,20 @@
+void main() {
+  // = (atribuição)
+  // -= (subtração e atribuição)
+  // /= (divisão e atribuição)
+  // %= (resto da divisão e atribuição)
+  // >>=
+  // ^=
+  // += (soma e atribuição)
+  // *= (multiplação e atribuição)
+  // ~/=
+  // <<=
+  // &=
+  // |=
+  // ??= (caso null faça atribuição)
+
+  var num1 = 2;
+  var num2 = 5;
+
+  print('$num1 >>= $num2: ${num1 >> num2}');
+}
