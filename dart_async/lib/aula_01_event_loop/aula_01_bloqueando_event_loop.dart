9c23d408c245a71b42f641528737d9d310d03583
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-10-23 23:17:21 -0300

    comment bug

--- dart_async/lib/aula_01_event_loop/aula_01_bloqueando_event_loop.dart
+++ dart_async/lib/aula_01_event_loop/aula_01_bloqueando_event_loop.dart
@@ -8,11 +8,11 @@ void main() {
 }
 
 String temperaturaAtual() {
-  sleep(const Duration(seconds: 3));
+  // sleep(const Duration(seconds: 3));
   return 'Está fazendo 30';
 }
 
 String previsaoAmanha() {
-  sleep(const Duration(seconds: 4));
+  // sleep(const Duration(seconds: 4));
   return 'Amanhã fará 34';
 }
