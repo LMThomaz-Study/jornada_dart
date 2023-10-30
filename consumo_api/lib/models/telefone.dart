39a7841b34746e0bfe74db87db9a76ecf49c408f
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-10-30 15:09:14 -0300

    Serialização e Desserialização de Modelos

--- consumo_api/lib/models/telefone.dart
+++ consumo_api/lib/models/telefone.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Telefone {
   int ddd;
   String telefone;
@@ -6,4 +8,41 @@ class Telefone {
     required this.ddd,
     required this.telefone,
   });
+
+  //
+  // Serialização
+  //
+
+  // método que pega nosso objeto Telefone e tranforma
+  //  em um objecto Map<String, dynamic>
+  // toMap() -> Passo 1
+  Map<String, dynamic> toMap() {
+    return {
+      'ddd': ddd,
+      'telefone': telefone,
+    };
+  }
+
+  // método que pega um Map<String, dynamic> e transforma
+  //  em um json pelo pacote dart:convert (jsonEncode)
+  // toJson() -> Passo 2
+  String toJson() => jsonEncode(toMap());
+
+  //
+  // Desserialização
+  //
+
+  // Passo 2
+  factory Telefone.fromMap(Map<String, dynamic> map) {
+    return Telefone(
+      ddd: map['ddd'],
+      telefone: map['telefone'],
+    );
+  }
+
+  // Passo 1
+  factory Telefone.fromJson(String json) {
+    final jsonMap = jsonDecode(json);
+    return Telefone.fromMap(jsonMap);
+  }
 }
