758cd0bd893be90d1dcb2d08d063550f61c06a18
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-11-01 13:29:31 -0300

    Buscando aluno por id

--- lab_cli/lib/src/commands/students/students_command.dart
+++ lab_cli/lib/src/commands/students/students_command.dart
@@ -2,6 +2,7 @@ import 'package:args/command_runner.dart';
 
 import '../../repositories/student_repository.dart';
 import 'subcommands/find_all_command.dart';
+import 'subcommands/find_by_id_command.dart';
 
 class StudentsCommand extends Command {
   @override
@@ -14,5 +15,6 @@ class StudentsCommand extends Command {
     final studentRepository = StudentRepository();
 
     addSubcommand(FindAllCommand(studentRepository));
+    addSubcommand(FindByIdCommand(studentRepository));
   }
 }
