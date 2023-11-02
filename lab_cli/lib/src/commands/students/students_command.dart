b0b231497d4201c464215c281e4add0195c67b40
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2023-11-01 21:44:33 -0300

    Desafio deletando aluno

--- lab_cli/lib/src/commands/students/students_command.dart
+++ lab_cli/lib/src/commands/students/students_command.dart
@@ -1,6 +1,7 @@
 import 'package:args/command_runner.dart';
 
 import '../../repositories/student_repository.dart';
+import 'subcommands/delete_command.dart';
 import 'subcommands/find_all_command.dart';
 import 'subcommands/find_by_id_command.dart';
 import 'subcommands/insert_command.dart';
@@ -20,5 +21,6 @@ class StudentsCommand extends Command {
     addSubcommand(FindByIdCommand(studentRepository));
     addSubcommand(InsertCommand(studentRepository));
     addSubcommand(UpdateCommand(studentRepository));
+    addSubcommand(DeleteCommand(studentRepository));
   }
 }
---
+++ lab_cli/lib/src/commands/students/subcommands/delete_command.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+
+import '../../../repositories/student_repository.dart';
+
+class DeleteCommand extends Command {
+  final StudentRepository studentRepository;
+
+  @override
+  String get description => 'Delete student';
+
+  @override
+  String get name => 'delete';
+
+  DeleteCommand(this.studentRepository) {
+    argParser.addOption('id', help: 'Student ID', abbr: 'i');
+  }
+
+  @override
+  Future<void> run() async {
+    final idArg = argResults?['id'];
+    final id = int.tryParse(idArg);
+
+    if (idArg == null) {
+      print('Por favor informe o id do aluno com o comando --id=0 oi -i 0');
+      return;
+    } else if (id == null) {
+      print('Por favor informe o id válido sendo apenas números');
+      return;
+    }
+
+    final student = await studentRepository.findById(id);
+
+    print('Deseja realmente deletar o aluno ${student.name}? (S ou N)');
+    final confirmDelete = stdin.readLineSync();
+    if (confirmDelete?.toLowerCase() != 's') {
+      print('Operação de remoção cancelada');
+      return;
+    }
+
+    await studentRepository.deleteById(id);
+    print('O aluno ${student.name} foi removido com sucesso');
+  }
+}
